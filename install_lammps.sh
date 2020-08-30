c586b3c9bb9a1ed10d67eadf40a6b17a5f1ec413
acyang <acyang0903@gmail.com>
2020-08-31 00:23:35 +0800

    Add files via upload

---
+++ install_lammps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Now clone your favorite Lammps code
+cd /root
+git clone https://github.com/lammps/lammps.git --depth=1
+cd /root/lammps/src
+
+#Now build Lammps
+make yes-user-omp
+make serial -j20
